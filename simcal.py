e6f71a001b31368a5b70a5bec8a6dbe93c126ee0
SeniorCodeLabs <aipythoncodinglab@gmail.com>
2026-04-23 09:32:35 +0530

    division added

--- simcal.py
+++ simcal.py
@@ -8,10 +8,16 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
 print("Simple Calculator")
 x = int(input("Enter first number: "))
 y = int(input("Enter second number: "))
 
+print("Division:", divide(x, y))
 print("Multiplication:", multiply(x, y))
 print("Addition:", add(x, y))
 print("Subtraction:", subtract(x, y))
