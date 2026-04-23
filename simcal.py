918211bb95703246a576529090a347d68a55f4a8
SeniorCodeLabs <aipythoncodinglab@gmail.com>
2026-04-23 09:12:23 +0530

    added simple calculator code

---
+++ simcal.py
@@ -0,0 +1,13 @@
+#simple calculator in python
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+print("Simple Calculator")
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Addition:", add(x, y))
+print("Subtraction:", subtract(x, y))
