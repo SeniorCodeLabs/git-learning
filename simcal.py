2ee7cf66b16a410124f8b7fcb68486ebef0a1e30
SeniorCodeLabs <aipythoncodinglab@gmail.com>
2026-04-23 09:18:06 +0530

    multiplication added

--- simcal.py
+++ simcal.py
@@ -5,9 +5,13 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 print("Simple Calculator")
 x = int(input("Enter first number: "))
 y = int(input("Enter second number: "))
 
+print("Multiplication:", multiply(x, y))
 print("Addition:", add(x, y))
 print("Subtraction:", subtract(x, y))
